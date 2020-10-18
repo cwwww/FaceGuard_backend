9d4351fe29e61e0db56fd89d221340129103c183
cwwww <535348244@qq.com>
2020-10-18 10:39:45 +0800

    working successfully, may need to change the image url in database later

--- faceguard/views.py
+++ faceguard/views.py
@@ -5,10 +5,14 @@ import base64
 from rest_framework.mixins import Response
 from rest_framework.views import APIView
 from .models import User
-
+from .cropFaces import crop
 
 
 current_path = os.path.dirname(__file__)
+img_path = './images/'
+dlib_path = './model/shape_predictor_68_face_landmarks.dat'
+crop_path = './images_crop/'
+rectangle_path = './images_rectangle/'
 
 def response_success_200(res={}):
     """
@@ -35,25 +39,23 @@ def response_failed_400():
     return Response(response, status=400)
 
 
-
 class SaveUserInfo(APIView):
     def post(self, request):
-        response = {}
-        try:
-            data = request.data
-            print(data)
-            img_data = base64.b64decode(data["img"])
-
+        data = request.data
+        print(data)
+        img_data = base64.b64decode(data["img"])
+        last_id = User.objects.all().order_by("-user_id")[:1]
+        img_name = str(last_id[0].user_id + 1) + '.jpg'
+        img_url = img_path + img_name
+        with open(img_url, 'wb') as f:
+            f.write(img_data)
+        print(img_name)
+        has_face = crop(img_path + img_name,crop_path, rectangle_path,dlib_path)
+        print(has_face)
+        if has_face:
             new_user = User(name=request.data['name'],address=request.data['address'],sex= request.data['sex'],
                             age=request.data['age'],job=request.data['job'])
             new_user.save()
-            img_url = './images/' + str(new_user.user_id) + '.jpg'
 
-            with open(img_url, 'wb') as f:
-                f.write(img_data)
-
-        except  Exception as e:
-            response['msg'] = str(e)
-            response['error_num'] = 1
-            print(str(e))
-        return Response(response, status=200)
+        res = {"has_face": has_face}
+        return response_success_200(res)
